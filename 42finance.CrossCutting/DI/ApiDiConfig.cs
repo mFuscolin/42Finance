44dd854598de82296692d69bca347b0a6967b06d
luciano.schirmer <luciano.schirmer@fulltechpro.com.br>
2021-10-14 20:16:03 -0300

    feature-estrutura-di-mapper-camadas fazendo arquitetura

---
+++ 42finance.CrossCutting/DI/ApiDiConfig.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _42finance.CrossCutting.DI
+{
+    public static class ApiDiConfig
+    {
+        public static void AddDependencyInjection(this IServiceCollection services, bool forWorkers = false, bool ignoreIdentity = false)
+        {
+            ///ConfigureDomainServices(services, forWorkers);
+        }
+
+		private static void ConfigureServices(IServiceCollection services, bool forWorkers)
+		{
+			if (!forWorkers)
+			{
+				//services.AddTransient<AutenticacaoSsoService>();
+			}
+
+			//services.AddTransient<SafeEfDbService>();
+		}
+	}
+}
