f21aea21d24053a458d33ad8985daf959b76bca2
luciano.schirmer <luciano.schirmer@fulltechpro.com.br>
2021-10-18 16:32:15 -0300

    criado entidades

---
+++ 42finance.Domain/Entities/Email/EnvioEmail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _42finance.Domain.Entities.Email
+{
+    public class EnvioEmail
+    {		
+		public string De { get; set; }
+
+		public string Para { get; set; }
+
+		public string Copia { get; set; }
+
+		public string CopiaOculta { get; set; }
+
+		public DateTime DtInclusao { get; set; }		
+		public string Assunto { get; set; }
+
+		public string Texto { get; set; }
+
+		public bool Enviado { get; set; }
+
+		public DateTime? DataEnvio { get; set; }
+		
+		public string Erro { get; set; }
+		
+		public string ReplyTo { get; set; }
+	}
+}
